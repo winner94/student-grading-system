8e7ccfd03ddade32784e50d4b845848493b5d988
tkolecki <tkolecki@gmail.com>
2026-04-27 21:21:43 +0200

    Add grading loop and pass/fail output

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ students = {
     "Czesław" : 92
 }
 
+grades = dict()
 '''
 Points to grades:
 0-49 -> 2
@@ -43,6 +44,19 @@ def has_passed(grade):
 #   print(get_grade(54))        # expected 3
 #   print(get_grade(92))        # expected 5
 # dictionary: student --> grade
+for k, v in students.items():
+    grades[k] = get_grade(v)
+
 # print results
+for k, v in grades.items():
+    if has_passed(v):
+        print(f"{k} {v} - Passed")
+    else:
+        print(f"{k} {v} - Failed")
 # print who pass
 # print group average
+
+for k, v in students.items():
+    grades[k] = get_grade(v)
+
+#print(grades)
